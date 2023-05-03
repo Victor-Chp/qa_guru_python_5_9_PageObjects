47b7b45851a6178514031025a5feb9f033d71fef
Victor Chp <qatester906@gmail.com>
2023-05-03 23:17:14 +0300

    high-level-step version 1.0

--- demoqa_tests/pages/registration_page.py
+++ demoqa_tests/pages/registration_page.py
@@ -45,58 +45,25 @@ class RegistrationPage:
         ).click()
         browser.element('#submit').press_enter()
 
-    #
-    # def fill_hobbie(self, value):
-    #     browser.all('#hobbiesWrapper .custom-checkbox').element_by(
-    #         have.exact_text(value)
-    #     ).click()
-    #
-    # def upload_picture(self, name):
-    #     browser.element('#uploadPicture').send_keys(resource.path(name))
-    #
-    # def fill_address(self, value):
-    #     browser.element('#currentAddress').send_keys(value)
-    #
-    # def fill_state(self, name):
-    #     self.state.perform(command.js.scroll_into_view)
-    #     self.state.click()
-    #     browser.all('[id^=react-select][id*=option]').element_by(
-    #         have.exact_text(name)
-    #     ).click()
-    #
-    # def fill_city(self, name):
-    #     browser.element('#city').click()
-    #     browser.all('[id^=react-select][id*=option]').element_by(
-    #         have.exact_text(name)
-    #     ).click()
-    #
-    # def submit(self):
-    #     browser.element('#submit').press_enter()
-    #
-    # def should_registered_user_with(
-    #     self,
-    #     full_name,
-    #     email,
-    #     gender,
-    #     user_number,
-    #     date_of_birth,
-    #     subjects,
-    #     hobbies,
-    #     picture,
-    #     address,
-    #     state_city,
-    # ):
-    #     browser.all('.table-responsive td:nth-child(2)').should(
-    #         have.exact_texts(
-    #             full_name,
-    #             email,
-    #             gender,
-    #             user_number,
-    #             date_of_birth,
-    #             subjects,
-    #             hobbies,
-    #             picture,
-    #             address,
-    #             state_city,
-    #         )
-    #     )
+    def should_have_registered(self, student):
+        full_name = f'{student.first_name} {student.last_name}'
+        date_of_birth = f'{student.date_of_birth.day} {student.date_of_birth.strftime("%B")},{student.date_of_birth.year}'
+        subjects = ', '.join(student.subjects)
+        hobbies = ', '.join(student.hobbies)
+        state_city = f'{student.state} {student.city}'
+
+        browser.all('.table-responsive td:nth-child(2)').should(
+            have.exact_texts(
+                full_name,
+                student.email,
+                student.genders,
+                str(student.phone_number),
+                date_of_birth,
+                subjects,
+                hobbies,
+                student.upload_filename,
+                student.current_address,
+                state_city
+            )
+        )
+
