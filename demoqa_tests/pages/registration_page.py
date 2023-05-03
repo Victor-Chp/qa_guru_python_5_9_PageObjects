73f99be437b48f3c6be11490d51eefa8ab83ff19
Victor Chp <qatester906@gmail.com>
2023-05-03 18:36:47 +0300

    high-level-step version 0.1

--- demoqa_tests/pages/registration_page.py
+++ demoqa_tests/pages/registration_page.py
@@ -3,89 +3,95 @@ from demoqa_tests import resource
 
 
 class RegistrationPage:
-    def __init__(self):
-        self.state = browser.element('#state')
-
     def open(self):
         browser.open('/automation-practice-form')
         # return self
 
-    def fill_first_name(self, value):
-        browser.element('#firstName').set(value)
-        # return self
-
-    def fill_last_name(self, value):
-        browser.element('#lastName').set(value)
-
-    def fill_email(self, value):
-        browser.element('#userEmail').set(value)
-
-    def fill_gender(self, value):
-        browser.element(f'[name=gender][value={value}]+label').click()
-
-    def fill_phone_number(self, value):
-        browser.element('#userNumber').set(value)
-
-    def fill_date_of_birth(self, year, month, day):
+    def register(self, student):
+        # WHEN
+        browser.element('#firstName').set(student.first_name)
+        browser.element('#lastName').set(student.last_name)
+        browser.element('#userEmail').set(student.email)
+        browser.element(f'[name=gender][value={student.genders}]+label').click()
+        browser.element('#userNumber').set(student.phone_number)
         browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__year-select').send_keys(year)
-        browser.element('.react-datepicker__month-select').send_keys(month)
-        browser.element(f'.react-datepicker__day--0{day}').click()
-
-    def fill_subject(self, value):
-        browser.element('#subjectsInput').send_keys(value).press_enter()
-
-    def fill_hobbie(self, value):
-        browser.all('#hobbiesWrapper .custom-checkbox').element_by(
-            have.exact_text(value)
-        ).click()
-
-    def upload_picture(self, name):
-        browser.element('#uploadPicture').send_keys(resource.path(name))
-
-    def fill_address(self, value):
-        browser.element('#currentAddress').send_keys(value)
-
-    def fill_state(self, name):
-        self.state.perform(command.js.scroll_into_view)
-        self.state.click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(name)
-        ).click()
-
-    def fill_city(self, name):
-        browser.element('#city').click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(name)
-        ).click()
-
-    def submit(self):
-        browser.element('#submit').press_enter()
-
-    def should_registered_user_with(
-        self,
-        full_name,
-        email,
-        gender,
-        user_number,
-        date_of_birth,
-        subjects,
-        hobbies,
-        picture,
-        address,
-        state_city,
-    ):
-        browser.all('.table-responsive td:nth-child(2)').should(
-            have.exact_texts(
-                full_name,
-                email,
-                gender,
-                user_number,
-                date_of_birth,
-                subjects,
-                hobbies,
-                picture,
-                address,
-                state_city,
-            )
+        browser.element('.react-datepicker__year-select').send_keys(
+            student.date_of_birth.year
         )
+        browser.element('.react-datepicker__month-select').send_keys(
+            student.date_of_birth.strftime('%B')
+        )
+        browser.element(f'.react-datepicker__day--0{student.date_of_birth.day}').click()
+
+        for subject in student.subjects:
+            browser.element('#subjectsInput').send_keys(subject).press_tab()
+        for hobby in student.hobbies:
+            browser.all('#hobbiesWrapper .custom-checkbox').element_by(
+                have.exact_text(hobby)
+            ).click()
+        browser.element('#uploadPicture').send_keys(
+            resource.path(student.upload_filename)
+        )
+
+        browser.element('#currentAddress').send_keys(student.current_address)
+        browser.element('#state').perform(command.js.scroll_into_view)
+        browser.element('#state').click()
+        browser.all('[id^=react-select][id*=option]').element_by(
+            have.exact_text(student.state)
+        ).click()
+
+    #
+    # def fill_hobbie(self, value):
+    #     browser.all('#hobbiesWrapper .custom-checkbox').element_by(
+    #         have.exact_text(value)
+    #     ).click()
+    #
+    # def upload_picture(self, name):
+    #     browser.element('#uploadPicture').send_keys(resource.path(name))
+    #
+    # def fill_address(self, value):
+    #     browser.element('#currentAddress').send_keys(value)
+    #
+    # def fill_state(self, name):
+    #     self.state.perform(command.js.scroll_into_view)
+    #     self.state.click()
+    #     browser.all('[id^=react-select][id*=option]').element_by(
+    #         have.exact_text(name)
+    #     ).click()
+    #
+    # def fill_city(self, name):
+    #     browser.element('#city').click()
+    #     browser.all('[id^=react-select][id*=option]').element_by(
+    #         have.exact_text(name)
+    #     ).click()
+    #
+    # def submit(self):
+    #     browser.element('#submit').press_enter()
+    #
+    # def should_registered_user_with(
+    #     self,
+    #     full_name,
+    #     email,
+    #     gender,
+    #     user_number,
+    #     date_of_birth,
+    #     subjects,
+    #     hobbies,
+    #     picture,
+    #     address,
+    #     state_city,
+    # ):
+    #     browser.all('.table-responsive td:nth-child(2)').should(
+    #         have.exact_texts(
+    #             full_name,
+    #             email,
+    #             gender,
+    #             user_number,
+    #             date_of_birth,
+    #             subjects,
+    #             hobbies,
+    #             picture,
+    #             address,
+    #             state_city,
+    #         )
+    #     )
--- tests/test_toolsqa_practice_form.py
+++ tests/test_toolsqa_practice_form.py
@@ -1,29 +1,48 @@
+from datetime import date
+
 from selene import browser, have
 from demoqa_tests import resource
+from demoqa_tests.data.users import User, Gender, Subject, Hobby
 from demoqa_tests.pages.registration_page import RegistrationPage
 
 
 def test_form_filling_submitting():
+    student = User(
+        first_name='Василий',
+        last_name='Алибабаев',
+        email='alibabavas@gmail.com',
+        genders=Gender.male.value,
+        phone_number=9093335555,
+        date_of_birth=date(1980, 7, 11),
+        subjects=[Subject.english.value, Subject.chemistry.value, Subject.arts.value],
+        hobbies=[Hobby.music.value, Hobby.reading.value],
+        upload_filename='account.png',
+        current_address='проспект Революции 285 - 45',
+        state='Uttar Pradesh',
+        city='Agra',
+    )
+
     registration_page = RegistrationPage()
     registration_page.open()
 
     # WHEN
-    registration_page.fill_first_name('Василий')
-    registration_page.fill_last_name('Алибабаев')
-    registration_page.fill_email('alibabavas@gmail.com')
-    registration_page.fill_gender('Male')
-    registration_page.fill_phone_number('9093335555')
-    registration_page.fill_date_of_birth('1980', 'July', '11')
-
-    registration_page.fill_subject('English')
-    registration_page.fill_subject('Accounting')
-    registration_page.fill_hobbie('Music')
-    registration_page.upload_picture('account.png')
-
-    registration_page.fill_address('проспект Революции 285 - 45')
-    registration_page.fill_state('Uttar Pradesh')
-    registration_page.fill_city('Agra')
-    registration_page.submit()
+    registration_page.register(student)
+    # registration_page.fill_first_name('Василий')
+    # registration_page.fill_last_name('Алибабаев')
+    # registration_page.fill_email('alibabavas@gmail.com')
+    # registration_page.fill_gender('Male')
+    # registration_page.fill_phone_number('9093335555')
+    # registration_page.fill_date_of_birth('1980', 'July', '11')
+    #
+    # registration_page.fill_subject('English')
+    # registration_page.fill_subject('Accounting')
+    # registration_page.fill_hobbie('Music')
+    # registration_page.upload_picture('account.png')
+    #
+    # registration_page.fill_address('проспект Революции 285 - 45')
+    # registration_page.fill_state('Uttar Pradesh')
+    # registration_page.fill_city('Agra')
+    # registration_page.submit()
 
     # THEN
     registration_page.should_registered_user_with(
